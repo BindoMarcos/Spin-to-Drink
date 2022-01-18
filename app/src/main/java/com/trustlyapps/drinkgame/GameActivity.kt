ec4743f3acebdb5e8f9408453b02308288d75375
MarcosBindo <marcos.bindo@intive.com>
2022-01-18 17:00:07 -0300

    Added: Screen with conditions (appears once time) Added: Few game in Ten seconds Added: 9 secs timer to main activity roulette button

---
+++ app/src/main/java/com/trustlyapps/drinkgame/GameActivity.kt
@@ -0,0 +1,159 @@
+package com.trustlyapps.drinkgame
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bluehomestudio.luckywheel.LuckyWheel
+import com.bluehomestudio.luckywheel.WheelItem
+import com.trustlyapps.drinkgame.diezSeg.DiezSeg
+import com.trustlyapps.drinkgame.quienEsMas.QuienEsMas
+import com.trustlyapps.drinkgame.yoNunca.YoNunca
+import java.util.*
+import kotlin.collections.ArrayList
+
+class GameActivity : AppCompatActivity() {
+
+    private var spin: ImageButton? = null
+    private var wheelItemsArray: MutableList<WheelItem> = ArrayList()
+
+    private var point: String? = null
+
+    private var luckywheel: LuckyWheel? = null
+
+    private var animacion: Animation? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        generarItems()
+        luckywheel = findViewById(R.id.luckywheel)
+
+        luckywheel?.addWheelItems(wheelItemsArray)
+
+        spin = findViewById(R.id.bSpin)
+
+        spin?.setOnClickListener {
+            spin?.isEnabled = false
+
+            val buttonTimer = Timer()
+            buttonTimer.schedule(object : TimerTask() {
+                override fun run() {
+                    runOnUiThread { spin?.isEnabled = true }
+                }
+            }, 9000)
+
+            rotacion()
+        }
+
+
+        luckywheel?.setLuckyWheelReachTheTarget {
+            if (point == "2" || point == "6" || point == "10") {
+                val intent = Intent(this, YoNunca::class.java)
+                startActivity(intent)
+            }
+            if (point == "3" || point == "7" || point == "11") {
+                val intent = Intent(this, QuienEsMas::class.java)
+                startActivity(intent)
+            }
+            if (point == "4" || point == "8" || point == "12") {
+                val intent = Intent(this, DiezSeg::class.java)
+                startActivity(intent)
+            }
+            if (point == "1") {
+                val intent = Intent(this, PopUpWindow::class.java)
+                intent.putExtra("popuptitle", "SHOT")
+                intent.putExtra(
+                    "popuptext",
+                    "Todos los jugadores (incluyendo el host), beben un trago"
+                )
+                intent.putExtra("popupbutton", "OK")
+                intent.putExtra("darkstatusbar", false)
+                startActivity(intent)
+            }
+            if (point == "5") {
+                val intent = Intent(this, PopUpWindow::class.java)
+                intent.putExtra("popuptitle", "CAMBIO DE HOST")
+                intent.putExtra(
+                    "popuptext",
+                    "El Actual HOST tendra que heredar su puesto al jugador que desee"
+                )
+                intent.putExtra("popupbutton", "OK")
+                intent.putExtra("darkstatusbar", false)
+                startActivity(intent)
+            }
+            if (point == "9") {
+                rotacion()
+                Toast.makeText(this, "Vuelve a girar la ruleta", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun rotacion() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.anim_boton)
+        val rnds = (1..12).random()
+        luckywheel?.rotateWheelTo(rnds)
+        point = rnds.toString()
+        spin?.startAnimation(animacion)
+    }
+
+    private fun generarItems() {
+        wheelItemsArray
+
+        val diezSeg =
+            WheelItem(Color.BLACK, BitmapFactory.decodeResource(resources, R.drawable.stopwatch))
+
+        val yoNunca =
+            WheelItem(
+                Color.BLACK,
+                BitmapFactory.decodeResource(resources, R.drawable.hand_yo_nunca)
+            )
+
+        val quienesmas =
+            WheelItem(
+                Color.BLACK,
+                BitmapFactory.decodeResource(resources, R.drawable.question_solid)
+            )
+
+        val host =
+            WheelItem(Color.BLACK, BitmapFactory.decodeResource(resources, R.drawable.host))
+
+        val reload =
+            WheelItem(Color.BLACK, BitmapFactory.decodeResource(resources, R.drawable.reload))
+
+        val shot =
+            WheelItem(
+                Color.BLACK,
+                BitmapFactory.decodeResource(resources, R.drawable.glass_shot_solid)
+            )
+
+        wheelItemsArray.add(shot)
+
+        wheelItemsArray.add(yoNunca)
+        wheelItemsArray.add(quienesmas)
+        wheelItemsArray.add(diezSeg)
+
+        wheelItemsArray.add(host)
+
+        wheelItemsArray.add(yoNunca)
+        wheelItemsArray.add(quienesmas)
+        wheelItemsArray.add(diezSeg)
+
+        wheelItemsArray.add(reload)
+
+        wheelItemsArray.add(yoNunca)
+        wheelItemsArray.add(quienesmas)
+        wheelItemsArray.add(diezSeg)
+    }
+
+
+}
+
+
--- app/src/main/java/com/trustlyapps/drinkgame/MainActivity.kt
+++ app/src/main/java/com/trustlyapps/drinkgame/MainActivity.kt
@@ -1,153 +1,45 @@
 package com.trustlyapps.drinkgame
 
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Color
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bluehomestudio.luckywheel.LuckyWheel
-import com.bluehomestudio.luckywheel.WheelItem
-import com.trustlyapps.drinkgame.diezSeg.DiezSeg
-import com.trustlyapps.drinkgame.quienEsMas.QuienEsMas
-import com.trustlyapps.drinkgame.yoNunca.YoNunca
+
 
 class MainActivity : AppCompatActivity() {
-
-    private var spin: ImageButton? = null
-    private var wheelItemsArray: MutableList<WheelItem> = ArrayList()
-
-    private var point: String? = null
-
-    private var luckywheel: LuckyWheel? = null
-
-    private var animacion: Animation? = null
+    private val prevStarted: String = "yes"
+    private var acceptConditions: String = ""
 
 
+    override fun onResume() {
+        super.onResume()
+
+        val bAccept = findViewById<Button>(R.id.bAccept)
+
+        val sharedpreferences: SharedPreferences =
+            getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+
+        if (!sharedpreferences.getBoolean(prevStarted, false)) {
+            val editor: SharedPreferences.Editor = sharedpreferences.edit()
+            editor.putBoolean(prevStarted, true)
+            editor.apply()
+
+            bAccept.setOnClickListener{
+                moveToMain()
+                acceptConditions = "Yes"
+            }
+        } else {
+            moveToMain()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        generarItems()
-        luckywheel = findViewById(R.id.luckywheel)
-
-        luckywheel?.addWheelItems(wheelItemsArray)
-        spin = findViewById(R.id.bSpin)
-
-        spin?.setOnClickListener {
-            rotacion()
-        }
-
-        val bReglas: Button= findViewById(R.id.bReglasMain)
-        bReglas.setOnClickListener{
-            val intent = Intent(this, Reglas::class.java)
-            startActivity(intent)
-        }
-
-        luckywheel?.setLuckyWheelReachTheTarget {
-            if (point == "2" || point == "6" || point == "10") {
-                val intent = Intent(this, YoNunca::class.java)
-                startActivity(intent)
-            }
-            if (point == "3" || point == "7" || point == "11") {
-                val intent = Intent(this, QuienEsMas::class.java)
-                startActivity(intent)
-            }
-            if (point == "4" || point == "8" || point == "12") {
-                val intent = Intent(this, DiezSeg::class.java)
-                startActivity(intent)
-            }
-            if (point == "1") {
-                val intent = Intent(this, PopUpWindow::class.java)
-                intent.putExtra("popuptitle", "SHOT")
-                intent.putExtra(
-                    "popuptext",
-                    "Todos los jugadores (incluyendo el host), beben un trago"
-                )
-                intent.putExtra("popupbutton", "OK")
-                intent.putExtra("darkstatusbar", false)
-                startActivity(intent)
-            }
-            if (point == "5") {
-                val intent = Intent(this, PopUpWindow::class.java)
-                intent.putExtra("popuptitle", "CAMBIO DE HOST")
-                intent.putExtra(
-                    "popuptext",
-                    "El Actual HOST tendra que heredar su puesto al jugador que desee"
-                )
-                intent.putExtra("popupbutton", "OK")
-                intent.putExtra("darkstatusbar", false)
-                startActivity(intent)
-            }
-            if (point == "9") {
-                rotacion()
-                Toast.makeText(this, "Vuelve a girar la ruleta", Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    private fun rotacion() {
-        animacion = AnimationUtils.loadAnimation(this, R.anim.anim_boton)
-        val rnds = (1..12).random()
-        luckywheel?.rotateWheelTo(rnds)
-        point =  rnds.toString()
-        spin?.startAnimation(animacion)
-    }
-
-    private fun generarItems() {
-        wheelItemsArray
-
-        val diezSeg =
-            WheelItem(Color.BLACK, BitmapFactory.decodeResource(resources, R.drawable.stopwatch))
-
-        val yoNunca =
-            WheelItem(
-                Color.BLACK,
-                BitmapFactory.decodeResource(resources, R.drawable.hand_yo_nunca)
-            )
-
-        val quienesmas =
-            WheelItem(
-                Color.BLACK,
-                BitmapFactory.decodeResource(resources, R.drawable.question_solid)
-            )
-
-        val host =
-            WheelItem(Color.BLACK, BitmapFactory.decodeResource(resources, R.drawable.host))
-
-        val reload =
-            WheelItem(Color.BLACK, BitmapFactory.decodeResource(resources, R.drawable.reload))
-
-        val shot =
-            WheelItem(
-                Color.BLACK,
-                BitmapFactory.decodeResource(resources, R.drawable.glass_shot_solid)
-            )
-
-        wheelItemsArray.add(shot)
-
-        wheelItemsArray.add(yoNunca)
-        wheelItemsArray.add(quienesmas)
-        wheelItemsArray.add(diezSeg)
-
-        wheelItemsArray.add(host)
-
-        wheelItemsArray.add(yoNunca)
-        wheelItemsArray.add(quienesmas)
-        wheelItemsArray.add(diezSeg)
-
-        wheelItemsArray.add(reload)
-
-        wheelItemsArray.add(yoNunca)
-        wheelItemsArray.add(quienesmas)
-        wheelItemsArray.add(diezSeg)
+        setContentView(R.layout.activity_reglas)
     }
 
 
+    private fun moveToMain(){
+        startActivity(Intent(this, GameActivity::class.java))
     }
-
-
+}
