f919585c375b48a01f94993f13a8ca25dcdb69ce
TrustlyApps <Bindomarcos13@gmail.com>
2022-01-21 16:04:18 -0300

    Game Finished
    to be added: 60 games in 10 seconds

--- app/src/main/java/com/trustlyapps/drinkgame/GameActivity.kt
+++ app/src/main/java/com/trustlyapps/drinkgame/GameActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.bluehomestudio.luckywheel.LuckyWheel
 import com.bluehomestudio.luckywheel.WheelItem
 import com.trustlyapps.drinkgame.diezSeg.DiezSeg
@@ -33,6 +36,10 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.GameToolbar)
+        setSupportActionBar(toolbar)
+
+
         generarItems()
         luckywheel = findViewById(R.id.luckywheel)
 
@@ -41,18 +48,8 @@ class GameActivity : AppCompatActivity() {
         spin = findViewById(R.id.bSpin)
 
         spin?.setOnClickListener {
-            spin?.isEnabled = false
-
-            val buttonTimer = Timer()
-            buttonTimer.schedule(object : TimerTask() {
-                override fun run() {
-                    runOnUiThread { spin?.isEnabled = true }
-                }
-            }, 9000)
-
-            rotacion()
+            spinRoulette()
         }
-
 
         luckywheel?.setLuckyWheelReachTheTarget {
             if (point == "2" || point == "6" || point == "10") {
@@ -82,26 +79,16 @@ class GameActivity : AppCompatActivity() {
                 val intent = Intent(this, PopUpWindow::class.java)
                 intent.putExtra("popuptitle", "CAMBIO DE HOST")
                 intent.putExtra(
-                    "popuptext",
-                    "El Actual HOST tendra que heredar su puesto al jugador que desee"
+                    "popuptext", "El Actual HOST tendra que heredar su puesto al jugador que desee"
                 )
                 intent.putExtra("popupbutton", "OK")
                 intent.putExtra("darkstatusbar", false)
                 startActivity(intent)
             }
             if (point == "9") {
-                rotacion()
-                Toast.makeText(this, "Vuelve a girar la ruleta", Toast.LENGTH_LONG).show()
+                spinRoulette()
             }
         }
-    }
-
-    private fun rotacion() {
-        animacion = AnimationUtils.loadAnimation(this, R.anim.anim_boton)
-        val rnds = (1..12).random()
-        luckywheel?.rotateWheelTo(rnds)
-        point = rnds.toString()
-        spin?.startAnimation(animacion)
     }
 
     private fun generarItems() {
@@ -153,6 +140,69 @@ class GameActivity : AppCompatActivity() {
         wheelItemsArray.add(diezSeg)
     }
 
+   /* private fun changeLanguage() {
+        var languageToLoad: String = ""
+
+        if(Locale.getDefault().language.contains("es")){
+            languageToLoad = "en"
+        }else if (Locale.getDefault().language.contains("en")){
+            languageToLoad = "es"
+        }
+
+        val locale = Locale(languageToLoad)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        this.resources
+            .updateConfiguration(config, this.resources.displayMetrics)
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+
+        Toast.makeText(this, Locale.getDefault().language, Toast.LENGTH_SHORT).show()
+    }*/
+
+    private fun rotacion() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.anim_boton)
+        val rnds = (1..12).random()
+        luckywheel?.rotateWheelTo(rnds)
+        point = rnds.toString()
+        spin?.startAnimation(animacion)
+    }
+
+    private fun buttonDelay() {
+        spin?.isEnabled = false
+
+        val buttonTimer = Timer()
+        buttonTimer.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread { spin?.isEnabled = true }
+            }
+        }, 9000)
+    }
+
+    private fun spinRoulette() {
+        buttonDelay()
+        rotacion()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.iChangeLang -> {
+                Toast.makeText(this, "Nuevos idiomas \n vendran pronto", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> {
+                return onOptionsItemSelected(item)
+            }
+        }
+    }
 
 }
 
